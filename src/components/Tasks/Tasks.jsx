130873d45f350952d211c0df98408f85722f99fd
neshanth <59483685+neshanth@users.noreply.github.com>
2022-08-15 08:27:44 +0530

    added edit icon and tasks

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -3,11 +3,29 @@ import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useState, useEffect } from "react";
+import api from "../../api/api";
+import EditIcon from "../Icons/EditIcon";
+import { Link } from "react-router-dom";
 
 function Tasks() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const getTasks = async () => {
+    try {
+      const response = await api.get("/api/tasks");
+      setTasks([...response.data.tasks]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <Container className="my-2">
-      <h2 className="text-center text--primary">Tasks</h2>
+      <h2 className="text-center text--primary my-2">Tasks</h2>
       <Container>
         <Row className="justify-content-center">
           <Col lg={3}>
@@ -18,15 +36,36 @@ function Tasks() {
           </Col>
         </Row>
       </Container>
-      <Table className="my-2" striped>
+      <Table className="my-2" striped bordered responsive>
         <thead>
           <tr>
             <th></th>
             <th className="text-center">Task</th>
+            <th className="text-center">Description</th>
             <th className="text-center">Due Date</th>
             <th className="text-center">Actions</th>
           </tr>
         </thead>
+        <tbody>
+          {tasks.map((task) => {
+            return (
+              <tr>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>{task.task}</td>
+                <td>{task.description}</td>
+                <td>{task.due_date}</td>
+                <td>
+                  <Link to="/">
+                    <EditIcon />
+                  </Link>
+                  <Link to="/"></Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </Table>
     </Container>
   );
