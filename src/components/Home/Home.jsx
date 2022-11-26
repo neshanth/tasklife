4c610c1e660dd0c8c9fcf39901f1afd9867c15b5
neshanth <59483685+neshanth@users.noreply.github.com>
2022-11-26 21:57:37 +0530

    added hero image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,8 +18,17 @@ function Home() {
   if (loading) return <Spinner />;
 
   return (
-    <div className="container home-wrapper">
-      <h2>Welcome To Tasklife</h2>
+    <div className="home-wrapper">
+      <div className="hero-wrapper">
+        <div className="hero-bg">
+          <h1 className="hero-title">Welcome to Tasklife</h1>
+          <p className="hero-secondary lead">Manage your tasks without any hassle</p>
+          <div className="hero-buttons d-flex justify-content-center flex-column">
+            <button className="btn btn--primary btn-login">Login</button>
+            <button className="btn btn-secondary btn-demo-login">Demo Login</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
