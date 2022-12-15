28d4cbba203249b33f8a8acd265d3c9a77cefcf8
neshanth <59483685+neshanth@users.noreply.github.com>
2022-12-15 02:11:49 +0000

    Build: (6659dd7) test

---
+++ precache-manifest.74009f628d2f59d0568f1a4ff47e9dbe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7745902f1e4ada31b720",
+    "url": "/static/css/main.e849d88d.chunk.css"
+  },
+  {
+    "revision": "7745902f1e4ada31b720",
+    "url": "/static/js/main.26ca2b5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0992d099007fab74352a",
+    "url": "/static/css/2.4276ffe7.chunk.css"
+  },
+  {
+    "revision": "0992d099007fab74352a",
+    "url": "/static/js/2.e4bfbb3b.chunk.js"
+  },
+  {
+    "revision": "c0e90fb53725fffe1fd3bb1bd0ef5985",
+    "url": "/static/media/hero.c0e90fb5.jpg"
+  },
+  {
+    "revision": "1eb0efb3ba284e40501ebeb9eef35d29",
+    "url": "/static/media/inter-v12-latin-regular.1eb0efb3.woff"
+  },
+  {
+    "revision": "a53a38a468b8b13846a1ed7d4072819c",
+    "url": "/static/media/inter-v12-latin-600.a53a38a4.woff"
+  },
+  {
+    "revision": "b486f818eb5bf76342cca1a4b877ad1c",
+    "url": "/static/media/tasklife__close.b486f818.svg"
+  },
+  {
+    "revision": "2976e2c5ad77ce0880d305e50dd2f0f4",
+    "url": "/index.html"
+  }
+];
