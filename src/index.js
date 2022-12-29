3374643464574bc78ae98f524017808cb478d2f7
neshanth <59483685+neshanth@users.noreply.github.com>
2022-12-29 11:23:30 +0530

    fixed mobile page for home

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/app.css";
+import "./styles/queries.css";
 import { UserProvider } from "./context/context";
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 import history from "./history/history";
