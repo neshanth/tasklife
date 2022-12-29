175df82a1c352451899da35453498ea2652bee8b
neshanth <59483685+neshanth@users.noreply.github.com>
2022-12-29 13:40:50 +0530

    fixed long taskname

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -8,13 +8,18 @@ import "./taskitem.css";
 const TaskItem = ({ taskData, updateTaskStatus, deleteTask, label }) => {
   const { task, due_date, id, status } = taskData;
   let todo_date = new Date(due_date);
+  let isTaskNameLong = false;
+
+  if (window.innerWidth <= 600) {
+    isTaskNameLong = task.length > 10 ? true : false;
+  }
 
   return (
     <>
       <div className="task-item d-flex align-items-center my-3">
         <Form.Check id="status" className="mx-2 status-checkbox" checked={status} onChange={() => updateTaskStatus(id)} />
         <div className="task">
-          <p className="mb-0 mx-3">{task}</p>
+          <p className="mb-0 mx-3">{isTaskNameLong ? `${task.substring(0, 10)}...` : task}</p>
         </div>
         {label && <div className={`task-label mx-3 ${status === 1 ? "completed" : "pending"}`}>{status === 1 ? "Completed" : "Pending"}</div>}
         <div className="due-date flex-grow-1">
