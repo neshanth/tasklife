622b738038cebdb101c9654719e8e9f6353f3589
neshanth <59483685+neshanth@users.noreply.github.com>
2024-09-28 18:39:45 +0530

    removed unused variables from context

--- src/context/context.js
+++ src/context/context.js
@@ -4,14 +4,6 @@ import useIsMobile from "../hooks/useIsMobile";
 const UserContext = React.createContext();
 
 const UserProvider = ({ children }) => {
-  const TASK_DATA = {
-    task: "",
-    description: "",
-    due_date: "",
-    tags: [],
-    id: "",
-    status: 0
-  }
   const [showMobileNav, setShowMobileNav] = useState(false);
   const [auth, setAuth] = useState(false);
   const [user, setUser] = useState({});
@@ -24,15 +16,10 @@ const UserProvider = ({ children }) => {
     date: "",
     tags: []
   })
-  const [taskData, setTaskData] = useState(TASK_DATA)
-  const [taskFormAction, setTaskFormAction] = useState('create')
   const isMobile = useIsMobile()
   const handleMobileNavToggle = () => {
     setShowMobileNav(!showMobileNav);
   };
-  const resetTaskData = () => {
-    setTaskData(TASK_DATA)
-  }
   const valueObj = {
     showMobileNav,
     setShowMobileNav,
@@ -52,11 +39,6 @@ const UserProvider = ({ children }) => {
     isMobile,
     filters,
     setFilters,
-    taskData,
-    setTaskData,
-    taskFormAction,
-    setTaskFormAction,
-    resetTaskData
   }
   return <UserContext.Provider value={valueObj}>{children}</UserContext.Provider>;
 };
