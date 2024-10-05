3d8d6add54d3aa9db7b1dc2bcd072d9a7a8a29ba
neshanth <59483685+neshanth@users.noreply.github.com>
2024-10-05 12:22:14 +0530

    fixed add task button on mobile

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,30 +1,27 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../Sidebar/Sidebar";
-import api from "../../api/api";
 import Spinner from "../Spinner/Spinner";
-import Footer from "../Footer/Footer";
 import useAppContext from "../../hooks/useAppContext";
 import MobileHeader from "../MobileHeader/MobileHeader";
 import "./mainContent.scss";
+import AddTask from "../AddTask/AddTask";
 
 function MainContent() {
-  const { show, setShow, setAuth, isMobile } = useAppContext();
-  const [loading, setLoading] = useState(false);
-  const handleClose = () => {
-    setShow(false);
-  };
+  const { isMobile } = useAppContext();
+  const [loading] = useState(false);
 
   if (loading) return <Spinner />;
 
   return (
-    <div className="main-content">
-      {isMobile && <MobileHeader />}
-      <div className="content-wrapper tl-border">
-        <Outlet />
+    <>
+      <div className="main-content">
+        {isMobile && <MobileHeader />}
+        <div className="content-wrapper tl-border">
+          <Outlet />
+        </div>
       </div>
-      {/* <Footer /> */}
-    </div>
+      {isMobile && <AddTask />}
+    </>
   );
 }
 
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -10,14 +10,12 @@ import BreadCrumb from "../BreadCrumb/BreadCrumb";
 import ContentInfo from "../MainContent/ContentInfo/ContentInfo";
 import Filters from "../Filters/Filters";
 import "./tasks.scss";
-import useIsMobile from "../../hooks/useIsMobile";
 
 function Tasks({ getTasks, loading, updateTaskStatus, handleTaskDelete, tasks, setTasks, handleTaskForm, showTaskForm, setShowTaskForm }) {
   const location = useLocation();
-  const isMobile = useIsMobile();
   const pendingTasks = tasks.filter((task) => task.status === 0);
   const completedTasks = tasks.filter((task) => task.status === 1);
-  const { setFetchData } = useAppContext();
+  const { setFetchData, isMobile } = useAppContext();
 
   useEffect(() => {
     getTasks();
@@ -48,7 +46,6 @@ function Tasks({ getTasks, loading, updateTaskStatus, handleTaskDelete, tasks, s
           {!isMobile && <AddTask />}
         </div>
       </div>
-      {isMobile && <AddTask />}
     </>
   );
 }
