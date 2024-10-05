c37550c70ddaa9e5ff63d454b5b75d9886fbb82e
neshanth <59483685+neshanth@users.noreply.github.com>
2024-10-05 12:09:49 +0530

    fixed position of add task button on mob

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { useEffect, useState, useRef } from "react";
+import { useEffect } from "react";
 import TaskItem from "../TaskItem/TaskItem";
 import Spinner from "../Spinner/Spinner";
 import { useLocation } from "react-router-dom";
-import "./tasks.scss";
 import AddTask from "../AddTask/AddTask";
 import { renderToast } from "../../utils/utils";
 import useAppContext from "../../hooks/useAppContext";
 import BreadCrumb from "../BreadCrumb/BreadCrumb";
 import ContentInfo from "../MainContent/ContentInfo/ContentInfo";
 import Filters from "../Filters/Filters";
-import TaskForm from "../TaskForm/TaskForm";
+import "./tasks.scss";
+import useIsMobile from "../../hooks/useIsMobile";
 
 function Tasks({ getTasks, loading, updateTaskStatus, handleTaskDelete, tasks, setTasks, handleTaskForm, showTaskForm, setShowTaskForm }) {
   const location = useLocation();
+  const isMobile = useIsMobile();
   const pendingTasks = tasks.filter((task) => task.status === 0);
   const completedTasks = tasks.filter((task) => task.status === 1);
-  const { fetchData, setFetchData } = useAppContext();
+  const { setFetchData } = useAppContext();
 
   useEffect(() => {
     getTasks();
@@ -32,27 +33,23 @@ function Tasks({ getTasks, loading, updateTaskStatus, handleTaskDelete, tasks, s
   }
 
   return (
-    <div className="tl-tasks">
-      <BreadCrumb page="Tasks" />
-      <div className="content-container">
-        <ContentInfo sectionHeading="Tasks" sectionInfo="Overview of All the Pending Tasks" />
-        <Filters pendingTasks={pendingTasks} completedTasks={completedTasks} />
-        <div className="tl-tasks__container">
-          {tasks.map((task) => (
-            <TaskItem handleTaskForm={handleTaskForm} handleTaskDelete={handleTaskDelete} key={task.id} taskInfo={task} updateTaskStatus={updateTaskStatus} />
-          ))}
-          {/* {showTaskForm && <TaskForm updateTaskStatus={updateTaskStatus} tasks={tasks} setShowTaskForm={setShowTaskForm} handleTaskForm={handleTaskForm} getTasks={getTasks} setTasks={setTasks} />} */}
+    <>
+      <div className="tl-tasks">
+        <BreadCrumb page="Tasks" />
+        <div className="content-container">
+          <ContentInfo sectionHeading="Tasks" sectionInfo="Overview of All the Pending Tasks" />
+          <Filters pendingTasks={pendingTasks} completedTasks={completedTasks} />
+          <div className="tl-tasks__container">
+            {tasks.map((task) => (
+              <TaskItem handleTaskForm={handleTaskForm} handleTaskDelete={handleTaskDelete} key={task.id} taskInfo={task} updateTaskStatus={updateTaskStatus} />
+            ))}
+            {/* {showTaskForm && <TaskForm updateTaskStatus={updateTaskStatus} tasks={tasks} setShowTaskForm={setShowTaskForm} handleTaskForm={handleTaskForm} getTasks={getTasks} setTasks={setTasks} />} */}
+          </div>
+          {!isMobile && <AddTask />}
         </div>
-        {<AddTask />}
       </div>
-
-      {/* <div className="tasks-in-progress dashboard-section">
-        <p className="dashboard-sub-heading">Completed ({completedTasks.length})</p>
-        {completedTasks.map((task) => (
-          <TaskItem key={task.id} taskData={task} updateTaskStatus={updateTaskStatus} handleTaskDelete={handleTaskDelete} />
-        ))}
-      </div> */}
-    </div>
+      {isMobile && <AddTask />}
+    </>
   );
 }
 
