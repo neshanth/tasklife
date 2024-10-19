b170db91266b6cc0a5776b35108e29fd07ab41cd
neshanth <59483685+neshanth@users.noreply.github.com>
2024-10-19 13:01:51 +0530

    updated filters page with DatePickerWrapper

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -2,20 +2,14 @@ import "./filters.scss";
 import useAppContext from "../../hooks/useAppContext";
 import Icons from "../Icons/Icons";
 import Search from "../Search/Search";
-import { forwardRef, useState } from "react";
-import DatePicker from "react-datepicker";
+import { useState } from "react";
 import TLModal from "../TLModal/TLModal";
+import DatePickerWrapper from "../DatePickerWrapper/DatePickerWrapper";
 
 const Filters = ({ pendingTasks, completedTasks }) => {
   const { filters, setFilters } = useAppContext();
   const [startDate, setStartDate] = useState(null);
   const [showFilters, setShowFilters] = useState(false);
-  const DatePickerInput = forwardRef(({ value, onClick, className }, ref) => (
-    <div className="tl-filters__date tl-border filters-padding" onClick={onClick} ref={ref}>
-      <Icons type="calendar" w="20" h="20" />
-      <button className={className}>{value ? value : "Date"}</button>
-    </div>
-  ));
 
   const handleFilterChange = (type, value) => {
     const currentFilters = { ...filters };
@@ -25,6 +19,11 @@ const Filters = ({ pendingTasks, completedTasks }) => {
 
   const handleFilterClose = () => {
     setShowFilters(false);
+  };
+
+  const handleDatePicker = (date) => {
+    setStartDate(date);
+    handleFilterChange("date", date);
   };
 
   return (
@@ -50,15 +49,7 @@ const Filters = ({ pendingTasks, completedTasks }) => {
         {showFilters && (
           <TLModal page="Filters" showClose={true} handleCloseBtnClick={handleFilterClose}>
             <div className="tl-filters__wrapper">
-              <DatePicker
-                selected={startDate}
-                onChange={(date) => {
-                  setStartDate(date);
-                  handleFilterChange("date", date);
-                }}
-                customInput={<DatePickerInput />}
-                isClearable
-              />
+              <DatePickerWrapper startDate={startDate} isClearable={true} handleDatePicker={handleDatePicker} />
               <div className="tl-filters__tags tl-border filters-padding">
                 <Icons type="tag" w="20" h="20" />
                 <button>Tags</button>
