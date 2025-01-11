729d6c647d1461e19af8b9c56de273fccdd5ca56
neshanth <59483685+neshanth@users.noreply.github.com>
2025-01-11 18:32:10 +0530

    fixed search focus style

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,22 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Icons from "../Icons/Icons";
 import "./search.scss";
 const Search = ({ search, handleSearch, isMobile = false }) => {
+  const [isInputFocused, setIsInputFocused] = useState(false);
   const inputFocusRef = useRef();
   const handleInputFocus = () => {
+    setIsInputFocused(true);
     inputFocusRef.current.focus();
   };
+  const handleInputBlur = () => {
+    setIsInputFocused(false);
+  };
   return (
-    <div className={`tl__search tl-border ${isMobile && "tl__search--mobile"}`} onClick={handleInputFocus}>
+    <div tabIndex={0} className={`tl__search tl-border ${isMobile ? "tl__search--mobile" : ""} ${isInputFocused ? "tl__search--focused" : ""}`} onClick={handleInputFocus}>
       <div className="tl__search-container">
         {!isMobile && <Icons type="search" w="25" h="25" />}
 
-        <input value={search} onChange={handleSearch} ref={inputFocusRef} type="text" placeholder="Search..." />
+        <input onFocus={() => handleInputFocus()} onBlur={() => handleInputBlur()} value={search} onChange={handleSearch} ref={inputFocusRef} type="text" placeholder="Search..." />
       </div>
     </div>
   );
